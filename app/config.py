ddfd6f54d121054288caa9ea58222aa7eb1c9726
soumya-1712 <121597272+soumya-1712@users.noreply.github.com>
2024-07-16 14:07:31 +0530

    change 16

--- app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('secretkey') or 'secretkeystring'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://username:password@localhost/task_management'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
